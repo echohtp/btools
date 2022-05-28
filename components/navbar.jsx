bd7edd63b427cd37765ebf35327a5f52244c7461
Jason Schorr <echohtp@gmail.com>
2022-05-28 13:03:55 -0400

    fix other bad props

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -17,7 +17,7 @@ export const Navbar = props => {
     <div className='navbar bg-base-100'>
       <div className='navbar-start'>
         <div className='dropdown'>
-          <label tabindex='0' className='btn btn-ghost lg:hidden'>
+          <label tabIndex='0' className='btn btn-ghost lg:hidden'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               className='w-5 h-5'
@@ -26,15 +26,15 @@ export const Navbar = props => {
               stroke='currentColor'
             >
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M4 6h16M4 12h8m-8 6h16'
               />
             </svg>
           </label>
           <ul
-            tabindex='0'
+            tabIndex='0'
             className='p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'
           >
             <li>
