764fa50facaa7a8166dac46cd4b3949deba0cd87
Jason Schorr <echohtp@gmail.com>
2022-07-01 20:19:25 -0400

    add burn (#2)
    * add burn
    
    * adjust function name

---
+++ pages/burn.tsx
@@ -0,0 +1,236 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import { Navbar } from '../components/navbar'
+import { useMemo, useState } from 'react'
+//@ts-ignore
+import {  TOKEN_PROGRAM_ID,  createBurnInstruction, createCloseAccountInstruction } from '@solana/spl-token'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { Transaction, PublicKey } from '@solana/web3.js'
+import { gql } from '@apollo/client'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import client from '../client'
+
+import { NftRow } from '../components/nftRow'
+import * as ga from '../lib/ga'
+
+enum transactionState {
+  NONE,
+  SENDING,
+  DONE
+}
+
+const Burn: NextPage = () => {
+  interface Owner {
+    address: string
+    associatedTokenAccountAddress: string
+  }
+  interface Nft {
+    name: string
+    address: string
+    description: string
+    image: string
+    mintAddress: string
+    owner: Owner
+  }
+
+  const { publicKey, signTransaction, connected } = useWallet()
+  const { connection } = useConnection()
+  const [nfts, setNfts] = useState<Nft[]>([])
+  const [sending, setSending] = useState<Nft[]>([])
+  const [to, setTo] = useState('')
+  const [search, setSearch] = useState('')
+
+  const burn = async (list: Nft[]) => {
+    if (!list || !connection || !publicKey || !signTransaction) {
+      console.log('returning')
+      return
+    }
+
+    if (!list.length) {
+      console.log('probably want to select some nfts')
+      return
+    }
+
+    const tx = new Transaction()
+
+    for (var i = 0; i < list.length; i++) {
+      tx.add(
+        createBurnInstruction(
+          new PublicKey(list[i].owner.associatedTokenAccountAddress),
+          new PublicKey(list[i].mintAddress),
+          publicKey,
+          1,
+          [],
+          TOKEN_PROGRAM_ID
+        )
+      )
+      tx.add(
+        createCloseAccountInstruction(
+          new PublicKey(list[i].owner.associatedTokenAccountAddress),
+          publicKey,
+          publicKey,
+          [],
+          TOKEN_PROGRAM_ID
+        )
+      )
+    }
+    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+    tx.feePayer = publicKey
+
+    let signed: Transaction | undefined = undefined
+
+    try {
+      signed = await signTransaction(tx)
+    } catch (e) {
+      toast(e.message)
+
+      return
+    }
+
+    let signature: string | undefined = undefined
+
+    try {
+      signature = await connection.sendRawTransaction(signed.serialize())
+      await connection.confirmTransaction(signature, 'confirmed')
+
+      toast.success('Transaction successful')
+      ga.event({
+        action: 'burn_success',
+        params: { count: sending.length }
+      })
+      sending.map(n => {
+        setNfts(nfts.filter(n => !sending.includes(n)))
+      })
+      setSending([])
+    } catch (e) {
+      toast.error(e.message)
+      ga.event({
+        action: 'burn_error',
+        params: { msg: e.message }
+      })
+    }
+  }
+
+  const GET_NFTS = gql`
+    query GetNfts($owners: [PublicKey!], $limit: Int!, $offset: Int!) {
+      nfts(owners: $owners, limit: $limit, offset: $offset) {
+        address
+        mintAddress
+        name
+        description
+        image
+        owner {
+          address
+          associatedTokenAccountAddress
+        }
+      }
+    }
+  `
+
+  useMemo(() => {
+    if (publicKey?.toBase58()) {
+      client
+        .query({
+          query: GET_NFTS,
+          variables: {
+            owners: [publicKey?.toBase58()],
+            offset: 0,
+            limit: 10000
+          }
+        })
+        .then(res => setNfts(res.data.nfts))
+    } else {
+      setNfts([])
+      setSending([])
+      setTo('')
+    }
+  }, [publicKey?.toBase58()])
+
+  return (
+    <div>
+      <Head>
+        <title>Burn!</title>
+        <meta name='description' content='Burn multiple NFTs at once!' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='drawer drawer-end'>
+        <input id='my-drawer' type='checkbox' className='drawer-toggle' />
+        <div className='drawer-content'>
+          <Navbar sending={sending} />
+          <div className='w-full mb-4'>
+            <input
+              type='text'
+              placeholder='Search...'
+              className='w-full input input-bordered input-secondary'
+              onChange={e => setSearch(e.target.value)}
+            />
+          </div>
+          <div className='container px-4'>
+            <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+              {nfts
+                .filter(n => n.name.includes(search))
+                .map(n => (
+                  <NftRow
+                    key={Math.random()}
+                    name={n.name}
+                    image={n.image}
+                    unselect={() => {
+                      setSending(sending.filter(item => item !== n))
+                    }}
+                    select={() => {
+                      setSending([...sending, n])
+                    }}
+                    selected={sending.includes(n)}
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+
+        <div className='drawer-side'>
+          <label htmlFor='my-drawer' className='drawer-overlay'></label>
+          <ul className='p-4 overflow-y-auto menu w-80 bg-base-100 text-base-content'>
+            {sending.map(n => (
+              <li key={Math.random()}>
+                <NftRow
+                  name={n.name}
+                  image={n.image}
+                  unselect={() => {
+                    setSending(sending.filter(item => item !== n))
+                  }}
+                  select={() => {
+                    setSending([...sending, n])
+                  }}
+                  selected={sending.includes(n)}
+                />
+              </li>
+            ))}
+
+            {sending.length > 0 ? (
+              <>
+                <li key={Math.random()}>
+                  <button
+                    id='btn-copy'
+                    className='block text-white btn btn-primary'
+                    onClick={() => {
+                      burn(sending)
+                    }}
+                  >
+                    Burn them!
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>Select some NFTs!</li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Burn
