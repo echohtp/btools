24c9239884dc47f0e080d965bc1602ee2d45163f
Jason Schorr <echohtp@gmail.com>
2022-07-16 14:36:18 -0400

    save state

--- pages/nftedit.tsx
+++ pages/nftedit.tsx
@@ -6,7 +6,6 @@ import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import { Transaction, PublicKey } from '@solana/web3.js'
 import { toast } from 'react-toastify'
 import * as ga from '../lib/ga'
-import { UploadFile } from 'antd/es/upload/interface'
 import { gql } from '@apollo/client'
 import 'react-toastify/dist/ReactToastify.css'
 import client from '../client'
@@ -16,8 +15,8 @@ import { Connection, clusterApiUrl } from '@solana/web3.js'
 import { NftRow } from '../components/nftRow'
 import { materialRenderers, materialCells } from '@jsonforms/material-renderers'
 import { JsonForms } from '@jsonforms/react'
-import { useForm, Resolver } from 'react-hook-form'
-import { JsonMetadata, JsonMetadataProperties, Nft } from '../types'
+import { useForm } from 'react-hook-form'
+import { JsonMetadata, Nft } from '../types'
 
 const NftEdit: NextPage = () => {
   const connection = new Connection(clusterApiUrl('mainnet-beta'))
@@ -28,19 +27,36 @@ const NftEdit: NextPage = () => {
   const [sending, setSending] = useState<Nft[]>([])
   const [search, setSearch] = useState('')
   const [fileList, setFileList] = useState<any[]>([])
-
+  const [tabSelected, setTabSelected] = useState(1)
   const [nftMetadata, setNftMetadata] = useState<JsonMetadata | null>(null)
+  const [nftChainData, setNftChainData] = useState<any>({})
+  const [nftNewChainData, setNewChainData] = useState<any>({})
 
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm()
-  const onSubmit = handleSubmit(data => console.log(data))
+  const updateMetaplex = handleSubmit(data => {
+    console.log('updateMetaplex', data)
+  })
+  const updateChain = handleSubmit(data => {
+    console.log('updateChain', data)
+  })
 
   const GET_NFTS = gql`
-    query GetNfts($owners: [PublicKey!], $limit: Int!, $offset: Int!) {
-      nfts(owners: $owners, limit: $limit, offset: $offset) {
+    query GetNfts(
+      $creators: [PublicKey!]
+      $updateAuthorities: [PublicKey!]
+      $limit: Int!
+      $offset: Int!
+    ) {
+      nfts(
+        creators: $creators
+        updateAuthorities: $updateAuthorities
+        limit: $limit
+        offset: $offset
+      ) {
         address
         mintAddress
         name
@@ -60,7 +76,8 @@ const NftEdit: NextPage = () => {
         .query({
           query: GET_NFTS,
           variables: {
-            owners: [publicKey?.toBase58()],
+            creators: [publicKey?.toBase58()],
+            updateAuthorities: [publicKey?.toBase58()],
             offset: 0,
             limit: 10000
           }
@@ -100,13 +117,29 @@ const NftEdit: NextPage = () => {
               type: 'string'
             },
             value: {
-              type: "string"
+              type: 'string'
             }
           }
         }
-      },
+      }
     }
   }
+
+  const chainSchema = {
+    type: 'object',
+    properties: {
+      name: {
+        type: 'string'
+      },
+      symbol: {
+        type: 'string'
+      },
+      updateAuthority: {
+        type: 'string'
+      }
+    }
+  }
+
   const uischema = {
     type: 'VerticalLayout',
     elements: [
@@ -134,7 +167,7 @@ const NftEdit: NextPage = () => {
       },
       {
         type: 'Control',
-        scope: '#/properties/category',
+        scope: '#/properties/category'
       },
       {
         type: 'Control',
@@ -142,14 +175,33 @@ const NftEdit: NextPage = () => {
         options: {
           showSortButtons: true
         }
+      }
+    ]
+  }
+
+  const chainUiSchema = {
+    type: 'VerticalLayout',
+    elements: [
+      ,
+      {
+        type: 'Control',
+        scope: '#/properties/name'
       },
+      {
+        type: 'Control',
+        scope: '#/properties/symbol'
+      },
+      {
+        type: 'Control',
+        scope: '#/properties/updateAuthority'
+      }
     ]
   }
 
   const [data, setData] = useState<any>({})
   let mdata = nftMetadata
-  
-  
+  let cdata = nftChainData
+
   return (
     <>
       <div>
@@ -190,10 +242,34 @@ const NftEdit: NextPage = () => {
                           .nfts()
                           .findByMint(new PublicKey(n.mintAddress))
 
-                        console.log(m_nft.metadata)
+                        console.log(m_nft)
+                        setNftChainData({
+                          updateAuthority: m_nft.updateAuthority,
+                          mint: m_nft.mint,
+                          name: m_nft.name,
+                          symbol: m_nft.symbol,
+                          uri: m_nft.uri,
+                          sellerFeeBasisPoints: m_nft.sellerFeeBasisPoints,
+                          creators: m_nft.creators,
+                          primarySaleHappened: m_nft.primarySaleHappened,
+                          isMutable: m_nft.isMutable,
+                          editionNonce: m_nft.editionNonce,
+                          tokenStandard: m_nft.tokenStandard,
+                          collection: m_nft.collection,
+                          uses: m_nft.uses
+                        })
                         setNftMetadata(m_nft.metadata)
-                        if (m_nft.metadata.properties && m_nft.metadata.properties.files)
-                            setFileList(m_nft.metadata.properties?.files?.map((f)=>({"uid": String(f.type), "id": f.uri, "name": String(f.uri)})))
+                        if (
+                          m_nft.metadata.properties &&
+                          m_nft.metadata.properties.files
+                        )
+                          setFileList(
+                            m_nft.metadata.properties?.files?.map(f => ({
+                              uid: String(f.type),
+                              id: f.uri,
+                              name: String(f.uri)
+                            }))
+                          )
                         //@ts-ignore
                         document.getElementById('my-modal-6').checked = true
                       }}
@@ -208,46 +284,120 @@ const NftEdit: NextPage = () => {
       <input type='checkbox' id='my-modal-6' className='modal-toggle' />
       <div className='modal modal-bottom sm:modal-middle'>
         <div className='modal-box'>
-          <h3 className='text-lg font-bold'>{nftMetadata?.name}</h3>
-          <JsonForms
-            schema={schema}
-            uischema={uischema}
-            data={mdata}
-            renderers={materialRenderers}
-            cells={materialCells}
-            validationMode={"NoValidation"}
-            onChange={({ data }) => {
-              setData(data)
-              console.log(data)
-            }}
-          />
-          <h3>Files</h3> 
-          <Upload
-            onRemove={e => setFileList(fileList.filter(f => f !== e))}
-            className='h-[2rem] w-[200px]'
-            fileList={fileList}
-            success={(resp: any, file: any) => {
-              console.log('resp', resp)
-              console.log('file', file)
-              setFileList([...fileList, {"uid": String(resp.type), "id": String(resp.type), "name": String(resp.uri)}])
-            }}
-          >
-            <div className='flex h-[2rem] flex-col text-center border'>
-              <p className=''>Add files</p>
+          <div className='grid place-items-center'>
+            <h3 className='text-lg font-bold'>{nftMetadata?.name}</h3>
+            <div className='tabs'>
+              {tabSelected === 1 ? (
+                <a
+                  className='tab tab-bordered tab-active'
+                  onClick={() => setTabSelected(1)}
+                >
+                  Metaplex JSON
+                </a>
+              ) : (
+                <a
+                  className='tab tab-bordered'
+                  onClick={() => setTabSelected(1)}
+                >
+                  Metaplex JSON
+                </a>
+              )}
+              {tabSelected === 2 ? (
+                <a
+                  className='tab tab-bordered tab-active'
+                  onClick={() => setTabSelected(2)}
+                >
+                  On Chain Data
+                </a>
+              ) : (
+                <a
+                  className='tab tab-bordered'
+                  onClick={() => setTabSelected(2)}
+                >
+                  On Chain Data
+                </a>
+              )}
             </div>
-          </Upload>
+            {tabSelected === 1 && (
+              <>
+                <JsonForms
+                  schema={schema}
+                  uischema={uischema}
+                  data={mdata}
+                  renderers={materialRenderers}
+                  cells={materialCells}
+                  validationMode={'NoValidation'}
+                  onChange={({ data }) => {
+                    setData(data)
+                    console.log(data)
+                  }}
+                />
+                <h3>Files</h3>
+                <Upload
+                  onRemove={e => setFileList(fileList.filter(f => f !== e))}
+                  className='h-[2rem] w-[200px]'
+                  fileList={fileList}
+                  success={(resp: any, file: any) => {
+                    console.log('resp', resp)
+                    console.log('file', file)
+                    setFileList([
+                      ...fileList,
+                      {
+                        uid: String(resp.type),
+                        id: String(resp.type),
+                        name: String(resp.uri)
+                      }
+                    ])
+                  }}
+                >
+                  <div className='flex h-[2rem] flex-col text-center border'>
+                    <p className=''>Add files</p>
+                  </div>
+                </Upload>
 
-          <div className='modal-action'>
-            <label
-              htmlFor='my-modal-6'
-              className='btn btn-secondary'
-              onClick={onSubmit}
-            >
-              Update
-            </label>
-            <label htmlFor='my-modal-6' className='btn btn-primary'>
-              Cancel
-            </label>
+                <div className='modal-action'>
+                  <label
+                    htmlFor='my-modal-6'
+                    className='btn btn-secondary'
+                    onClick={updateMetaplex}
+                  >
+                    Update
+                  </label>
+                  <label htmlFor='my-modal-6' className='btn btn-primary'>
+                    Cancel
+                  </label>
+                </div>
+              </>
+            )}
+            {tabSelected === 2 && (
+              <>
+                <JsonForms
+                  schema={chainSchema}
+                  uischema={chainUiSchema}
+                  data={cdata}
+                  renderers={materialRenderers}
+                  cells={materialCells}
+                  validationMode={'NoValidation'}
+                  onChange={({ data }) => {
+                    setNewChainData(data)
+                    console.log(data)
+                  }}
+                />
+
+                <div className='modal-action'>
+                  <label
+                    htmlFor='my-modal-6'
+                    className='btn btn-secondary'
+                    onClick={updateChain}
+                  >
+                    Update
+                  </label>
+                  <label htmlFor='my-modal-6' className='btn btn-primary'>
+                    Cancel
+                  </label>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
